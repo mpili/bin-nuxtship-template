254726b70ad602c089f6ba7cba7e602cc4f25076
Mauro Pili <mauro.pili@gmail.com>
2024-06-09 23:57:39 +0200

    prima pagina leaflet

---
+++ components/landing/Lmapois.vue
@@ -0,0 +1,213 @@
+<template>
+	<LMarker :lat-lng="[44.049114, 8.213017]" >
+		<LPopup>Satoshi Spritz Albenga</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.5187, 11.8845]" >
+		<LPopup>Arezzo Bitcoin Meetup</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.07462, 6.69888]" >
+		<LPopup>Satoshi Spritz Bardonecchia</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[46.13837528, 12.21704167]" >
+		<LPopup>Satoshi Spritz Belluno</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.6974512, 9.6766857]" >
+		<LPopup>BitcoinBergamo</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.69441368, 9.66842453]" >
+		<LPopup>Satoshi Spritz Bergamo</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.5614642, 8.0557548]" >
+		<LPopup>Bitcoin Biella</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.4987, 11.364]" >
+		<LPopup>Bitcoin Beer Bologna</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.4943668, 11.3417208]" >
+		<LPopup>Bitcoin Bologna & Satoshi Spritz</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.2955943, 8.5022665]" >
+		<LPopup>Bitcoin Hub Bosa</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.2955943, 8.5022665]" >
+		<LPopup>₿ is for BOSA</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.53993052, 10.21910323]" >
+		<LPopup>Bitcoin Brescia & Satoshispritz</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.5414877, 10.2132107]" >
+		<LPopup>Bitcoin Beer Brescia</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[39.21721, 9.11335]" >
+		<LPopup>Satoshi Spritz Cagliari</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[38.9075, 16.5962]" >
+		<LPopup>₿itcoin Beer Catanzaro</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[46.365271, 11.033176]" >
+		<LPopup>Satoshi Spritz Cles</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.36264, 9.68176]" >
+		<LPopup>Satoshi Spritz Crema Cremona Lodi</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.384477, 7.542671]" >
+		<LPopup>Satoshi Spritz Cuneo</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.2897798, 11.8774255]" >
+		<LPopup>Bitcoin Beer Faenza</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.83599085, 11.61868934]" >
+		<LPopup>Satoshi Spritz Ferrara</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.7695604, 11.2558136]" >
+		<LPopup>Satoshi Spritz Firenze</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.7632, 11.2033]" >
+		<LPopup>Bitcoin Firenze & Toscana</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.1844464, 12.135094]" >
+		<LPopup>Bitcoin Beer Forlì Cesena</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.6494354, 13.7681366]" >
+		<LPopup>Satoshi Spritz Friuli FVG</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[41.8399626, 15.5605492]" >
+		<LPopup>Bitcoin Beer Gargano</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.4046185, 8.9484919]" >
+		<LPopup>Bitcoin Genova</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.2922106, 13.9070363]" >
+		<LPopup>Bitcoin Beer Istria</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.8365, 10.52112]" >
+		<LPopup>Bitcoin Beer Lucca</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.8511, 10.507994]" >
+		<LPopup>Satoshi Spritz Lucca</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[46.003677, 8.951052]" >
+		<LPopup>Satoshi Spritz Lugano</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[35.886192, 14.4508814]" >
+		<LPopup>Bitcoin Beer Malta</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.15726772, 10.79277363]" >
+		<LPopup>Satoshi Spritz Mantova</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.61675973, 13.51887537]" >
+		<LPopup>Satoshi Spritz Marche</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.4469, 9.2174]" >
+		<LPopup>Bitcoin Beer Milano</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.46679408, 9.1903474]" >
+		<LPopup>Satoshi Spritz Milano </LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.4859, 10.92615487]" >
+		<LPopup>Satoshi Spritz Modena</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.8050923, 13.5329932]" >
+		<LPopup>Bitcoin Beer Monfalcone</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.5834, 9.2759]" >
+		<LPopup>Satoshi Spritz Monza</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.8517746, 14.2681244]" >
+		<LPopup>Satoshi Spritz Napoli</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.3248, 9.3366]" >
+		<LPopup>Bitcoin ₿eer Nuoro</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.31949, 9.31394]" >
+		<LPopup>Satoshi Spritz Nuoro</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.40692987, 11.87608719]" >
+		<LPopup>Satoshi Spritz Padova</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.7951762, 14.3694801]" >
+		<LPopup>Bitcoin Beer Paesi Vesuviani</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[38.11569725, 13.36235675]" >
+		<LPopup>Satoshi Spritz Palermo (e Sicilia Occidentale)</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.8005585, 10.3288969]" >
+		<LPopup>Bitcoin Parma</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.052621, 9.692984]" >
+		<LPopup>Satoshi Spritz Parma e Piacenza</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.90921, 12.9164]" >
+		<LPopup>Satoshi Spritz Pesaro</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.9628504, 12.6551973]" >
+		<LPopup>Bitcoin Pordenone</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.8926686, 11.1049362]" >
+		<LPopup>Bitcoin Beer Prato</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[41.12559562, 16.86737016]" >
+		<LPopup>Satoshi Spritz Puglia</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.699, 10.6297]" >
+		<LPopup>Satoshi Spritz Reggio Emilia</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.0678288, 12.5695158]" >
+		<LPopup>Satoshi Spritz Rimini</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[41.887914110221, 12.4838967567175]" >
+		<LPopup>Satoshi Spritz Roma</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.4185898, 12.2011731]" >
+		<LPopup>Bitcoin Beer Romagna</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[46.065088, 11.123858]" >
+		<LPopup>SatoshiSpritz Rovereto e Trento (TN)</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.07107288, 11.79007]" >
+		<LPopup>Satoshi Spritz Rovigo</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.2975603, 8.4645]" >
+		<LPopup>Satoshi Spritz Savona</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[37.0703, 15.2889]" >
+		<LPopup>Bitcoin Beer Siracusa</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[40.464361, 17.24703]" >
+		<LPopup>Satoshi Spritz Taranto</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.0732745, 7.68068748]" >
+		<LPopup>Satoshi Spritz Torino</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.3186614, 11.3305135]" >
+		<LPopup>Satoshi Spritz Toscana-Sud</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.6675457, 12.24507364]" >
+		<LPopup>Satoshi Spritz Treviso</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.6460821, 13.7769859]" >
+		<LPopup>Bitcoin Trieste</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[43.110717, 12.390828]" >
+		<LPopup>Satoshi Spritz Umbria</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.8182, 8.82733]" >
+		<LPopup>Satoshi Spritz Varese</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.43490484, 12.33845214]" >
+		<LPopup>Satoshi Spritz Venezia</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.4321393, 12.3346526]" >
+		<LPopup>Bitcoin in Venice</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.43839046, 10.99352685]" >
+		<LPopup>Satoshi Spritz Verona</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[45.54749699, 11.54597109]" >
+		<LPopup>Satoshi Spritz Vicenza</LPopup>
+	</LMarker>
+	<LMarker :lat-lng="[44.4836, 11.0131]" >
+		<LPopup>Satoshi Spritz Vignola</LPopup>
+	</LMarker>
+
+</template>
--- pages/leaflet.vue
+++ pages/leaflet.vue
@@ -7,26 +7,18 @@ const zoom = ref(6)
 </script>
 
 <template>
-	<div style="height:100vh; width:100vw">
-	  <LMap
-		ref="map"
-		:zoom="zoom"
-		:center="[47.21322, -1.559482]"
-	  >
-		<LTileLayer
-		  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-		  attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-		  layer-type="base"
-		  name="OpenStreetMap"
-		/>
-	  </LMap>
-	</div>
-</template>
-
-
-<style>
-body {
-margin: 0;
-}
-</style>
-  
+	<LMap
+	  style="height: 400px"
+	  :zoom="5"
+	  :center="[41.8879, 12.4838]"
+	  :use-global-leaflet="false"
+	>
+	  <LTileLayer
+		url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+		layer-type="base"
+		name="OpenStreetMap"
+	  />
+		<LandingLmapois/>
+	</LMap>
+  </template>
