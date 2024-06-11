2cc31fc3f143ddbf9cecedf95b779e686a663077
Mauro Pili <mauro.pili@gmail.com>
2024-06-11 23:18:31 +0200

    community con modulo nuxt leafnet

---
+++ components/comunita/Telegramlink.vue
@@ -0,0 +1,17 @@
+<script setup>
+interface Props {
+	titolo?: string;
+	icona?: string;
+	url?: string;
+}
+</script>
+
+<template>
+	<div>
+		<Icon :name="icona" size="24" class="mr-2" />
+		<span class="text-gray-400">{{ titolo }}</span>
+		<a :href="url" target="_blank" class="text-sm font-bold text-blue-600">
+			{{url}}
+		</a>
+	</div>	
+</template>
