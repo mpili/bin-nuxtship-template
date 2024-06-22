28b16a9c6e4c53f0d8783e7c434bc438dc69dbaf
Mauro Pili <mauro.pili@gmail.com>
2024-06-22 16:03:17 +0200

    tipo venue

---
+++ components/osn/Tipovenue.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(["tags"]);
+</script>
+
+<template>
+	<span  v-if="tags.amenity">{{ tags.amenity }} </span>
+	<span  v-else-if="tags.leisure">{{ tags.leisure }} </span>
+	<span  v-else-if="tags.shop">{{ tags.shop }} </span>
+	<span  v-else-if="tags.tourism">{{ tags.tourism }} </span>
+	<span  v-else-if="tags.office">{{ tags.office }} </span>
+	<span v-else></span>
+</template>
