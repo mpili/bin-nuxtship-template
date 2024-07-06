fbbb39fc7312a26ed3422b9bf2e1065b780a03c4
Mauro Pili <mauro.pili@gmail.com>
2024-07-06 13:06:25 +0200

    indirizzo con contact oltre add

--- components/osn/Page.vue
+++ components/osn/Page.vue
@@ -31,12 +31,20 @@ defineProps(["data"]);
     <h2>Indirizzo</h2>
     <div class="p-2">
       <p>
-        {{ data.tags["addr:street"] }}
-        <span v-if="data.tags['addr:housenumber']"
-          >, {{ data.tags["addr:housenumber"] }}</span
-        >
+        <span v-if="data.tags['addr:street']">
+          {{ data.tags["addr:street"] }}<span v-if="data.tags['addr:housenumber']">, {{ data.tags["addr:housenumber"] }}</span>
+        </span>
+        <span v-else-if="data.tags['contact:street']">
+          {{ data.tags["contact:street"] }}<span v-if="data.tags['contact:housenumber']">, {{ data.tags["contact:housenumber"] }}</span>
+        </span>
       </p>
-      <p>{{ data.tags["addr:postcode"] }} {{ data.tags["addr:city"] }}</p>
+      <p>
+        <span v-if="data.tags['addr:postcode']">{{ data.tags["addr:postcode"] }}</span>
+        <span v-else-if="data.tags['contact:postcode']">{{ data.tags["contact:postcode"] }}</span>
+        &nbsp;
+        <span v-if="data.tags['addr:city']">{{ data.tags["addr:city"] }}</span>
+        <span v-else-if="data.tags['contact:city']">{{ data.tags["contact:city"] }}</span>
+      </p>
     </div>
 
       <div
