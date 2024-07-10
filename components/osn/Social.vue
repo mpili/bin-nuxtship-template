de188c62c8315e12c393cf9b8139a7d50e189a62
Mauro Pili <mauro.pili@gmail.com>
2024-07-10 23:03:29 +0200

    social buttons

---
+++ components/osn/Social.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      required: true,
+    },
+    icona: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<template>
+	<p v-if="value">
+		<LandingSocial
+			:social="nome"
+			:value="value"
+			:icona="icona"
+		/>
+	</p>
+</template>
