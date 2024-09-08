b041afa8070214e8c2ca2bf0d0200398de5e7cd6
Mauro Pili <mauro.pili@gmail.com>
2024-09-08 23:12:31 +0200

    pagina localita con icone ai meetup

--- components/localita/Meetup.vue
+++ components/localita/Meetup.vue
@@ -7,10 +7,13 @@ defineProps(["data"]);
     <h2 class="text-xl">
       <NuxtLink to="/meetups"> Meetup </NuxtLink>
     </h2>
-    <div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-2 text-xl font-bold">
       <div v-for="t in data.telegram" class="py-2">
         <NuxtLink :href="`/c/${t.telegram}`" class="text-blue-500">
+          <div class="flex items-center rounded-full border shadow gap-6 p-2">
+          <NuxtImg :src="`/img/c/${t.telegram}.jpeg`" :alt="t.telegram" class="inline-block relative object-cover object-center !rounded-full w-20 h-20 rounded-lg" />
           {{ t.telegram }}
+          </div>
         </NuxtLink>
       </div>
     </div>
