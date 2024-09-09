7651c5b85060c855fbe598562190544884e88862
Mauro Pili <mauro.pili@gmail.com>
2024-09-09 20:32:29 +0200

    foto nella pagina della località

--- components/localita/Page.vue
+++ components/localita/Page.vue
@@ -15,48 +15,44 @@ useHead({
   title: props?.data?.name + " | Bitcoin Italia Network",
   link: [
     {
-      rel: 'canonical',
+      rel: "canonical",
       href: `https://bitcoinitalianetwork.com/l/${props?.data?.id}`,
     },
   ],
 });
-
 </script>
 
 <template>
-		<div>
-			
-			
-			<NuxtImg :src="`/img/l/${data.id}.jpg`" class="object-center aspect-square max-h-96" />
-			<LandingSectionhead>
-				<template v-slot:title>{{ data.name }}</template>
-				<template v-slot:desc>{{ data.name }}</template>
-			</LandingSectionhead>
-			<LandingBreadcrumb :voci="breadcrumb" />
-		</div>
-			
+  <div>
+    <LandingSectionhead>
+      <template v-slot:title>{{ data.name }}</template>
+      <template v-slot:desc>{{ data.name }}</template>
+    </LandingSectionhead>
+    <div class="grid place-items-center pt-4">
+      <NuxtImg :src="`/img/l/${data.id}.jpg`" class="aspect-square max-h-96 rounded-md" />
+    </div>
+    <LandingBreadcrumb :voci="breadcrumb" />
+  </div>
 
-	<div>
+  <LocalitaMeetup :data="data" />
 
-		<div class="md:w-full">
-			<LMap
-				style="height:224px"
-				:zoom="10"
-				:center="[data.lng, data.lat]"
-				:use-global-leaflet="false"
-			>
-				<LTileLayer
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-					layer-type="base"
-					name="OpenStreetMap"
-				/>
-				<ComunitaPoimarker v-for="poi of data.poi" :poi="poi" />
-			</LMap>
-		</div>
-	</div>
-	
-	<LocalitaMeetup :data="data" />
-	<ComunitaPoislist :data="data" />
-	<LandingDisclaimer />
+  <div class="md:w-full">
+    <LMap
+      style="height: 224px"
+      :zoom="10"
+      :center="[data.lng, data.lat]"
+      :use-global-leaflet="false"
+    >
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+      <ComunitaPoimarker v-for="poi of data.poi" :poi="poi" />
+    </LMap>
+  </div>
+
+  <ComunitaPoislist :data="data" />
+  <LandingDisclaimer />
 </template>
