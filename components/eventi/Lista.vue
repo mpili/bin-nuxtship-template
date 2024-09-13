8f53f5029cf6e577d05eba3458be5bebf03e80d7
Mauro Pili <mauro.pili@gmail.com>
2024-09-13 18:39:54 +0200

    eventi con foto e icone

--- components/eventi/Lista.vue
+++ components/eventi/Lista.vue
@@ -11,21 +11,46 @@ export default {
     formattaData(data) {
       const d = new Date(data);
       return d.toLocaleDateString("it-IT");
-    }
-  }
+    },
+  },
 };
 </script>
 
 <template>
-  <div v-for="evento of eventi" class="py-2 text-xl text-blue-700">
-    <NuxtLink :to="`/evento/${evento.id}`">
-      <div class="flex gap-4">
-        <Icon name="material-symbols:calendar-month" size="48" color="gray" />
-        <!-- <span>{{ formattaData(evento.data) }}</span> -->
-        <span class="pt-3">
-          {{ evento.titolo }}
-        </span>
-      </div>
-    </NuxtLink>
-  </div>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4">
+  
+    <div v-for="evento of eventi">
+      <NuxtLink :to="`/evento/${evento.id}`">
+        <div class="text-blue-700 max-w-[300px] py-2">
+          <div class="border rounded-xl bg-clip-border shadow-md grid">
+            <div class="pt-2 text-center font-semibold">{{ formattaData(evento.data) }}</div>
+            <div v-if="evento.titolo" class="text-sm p-2">
+              {{ evento.titolo }}
+            </div>
+            <div v-if="evento.img">
+              <img
+                class="rounded-md"
+                :src="
+                  'https://bin-dev.pockethost.io/api/files/eventi/' +
+                  evento.id +
+                  '/' +
+                  evento.img
+                "
+              />
+            </div>
+            <div
+              v-else-if="evento.id_comunita"
+              class="relative m-0 overflow-hidden bg-transparent rounded-none bg-clip-border"
+            >
+              <img :src="`/img/c/${evento.id_comunita}.jpeg`" />
+            </div>
+            <div v-if="evento.id_comunita" class="py-2 text-center font-semibold">
+              {{ evento.id_comunita }}
+            </div>
+          </div>
+        </div>
+      </NuxtLink>
+    </div>
+  
+</div>
 </template>
