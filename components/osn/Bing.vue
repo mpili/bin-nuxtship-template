89c2bc3092f5fab99cb7e019f9d0cee9d4da3e40
Mauro Pili <mauro.pili@gmail.com>
2024-09-21 23:02:58 +0200

    link a bing nella pagina del merchant

---
+++ components/osn/Bing.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    pending: {
+      required: true,
+    },
+    dbdata: {
+      required: true,
+    },
+	label: {
+		type: String,
+	}
+  },
+};
+</script>
+
+<template>
+  <span v-if="!pending" class="mt-8">
+    <span v-if="dbdata?.items[0]?.bing">
+      <a
+        :href="dbdata?.items[0]?.bing"
+        target="_blank"
+        rel="noopener"
+        class="text-green-600 font-bold"
+      >{{label}}</a>
+    </span>
+    <span v-else>{{label}}</span>
+  </span>
+  <span v-else>{{label}}</span>
+</template>
