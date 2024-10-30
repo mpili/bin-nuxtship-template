aec387bc50874b96fbd1c36f375072d74057ff15
Mauro Pili <mauro.pili@gmail.com>
2024-10-30 23:23:31 +0100

    tripadvisor, event on osn

---
+++ components/osn/Tripadvisor.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    pending: {
+      required: true,
+    },
+    dbdata: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <span v-if="!pending">
+    <span v-if="dbdata.items[0].tripadvisor" class="mt-8">
+      <OsnSocial
+        nome="tripadvisor"
+        icona="simple-icons:tripadvisor"
+        :value="dbdata.items[0].tripadvisor"
+      />
+    </span>
+  </span>
+</template>
