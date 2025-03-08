d4530e95b095ab9044617d152e67217b149d929e
Mauro Pili <mauro.pili@gmail.com>
2025-03-08 18:45:25 +0100

    Aggiornati i dati del menu principale con nuovi servizi e rimosso il sottomenu obsoleto. Aggiunti nuovi file JSON per la distribuzione organizzata e aggiornate le immagini. Modificata la visualizzazione delle categorie nella lista e nel conteggio.

--- components/ost/Pagecountag.vue
+++ components/ost/Pagecountag.vue
@@ -16,15 +16,18 @@ export default {
 <template>
   <div class="flex flex-wrap py-1">
     <div>
-      <NuxtLink
-        :href="`/ost/${tipo}/${tag[tipo]}`"
-        class="text-blue-600 p-2"
-      >
-        <OsnTraducitag :tag="tipo" :value="tag[tipo]" />
+      <NuxtLink :href="`/ost/${tipo}/${tag[tipo]}`" class="text-blue-600 p-2">
+        <div class="indicator">
+          <span class="indicator-item badge badge-primary">{{
+            tag.count
+          }}</span>
+          <button class="btn">
+            <OsnTraducitag :tag="tipo" :value="tag[tipo]" />
+          </button>
+        </div>
       </NuxtLink>
     </div>
 
     <!-- <div class="bg-blue-100 text-blue-600 text-xs rounded-full py-1 px-2 mx-2"> -->
-    <div class="badge badge-primary">{{ tag.count }}</div>
   </div>
 </template>
