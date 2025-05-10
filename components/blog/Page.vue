31a6366f8c8a4a4712a6bdf936a2a6e27b1a75ec
Mauro Pili <mauro.pili@gmail.com>
2025-05-10 17:04:32 +0200

    blog components, updated POI

---
+++ components/blog/Page.vue
@@ -0,0 +1,18 @@
+<script setup>
+const url_api =
+  "https://bin-dev.pockethost.io/api/collections/blog/records?sort=data";
+
+const { pending, data, error } = await useLazyFetch(url_api, {
+  lazy: true,
+  server: false,
+});
+</script>
+
+<template>
+  <div v-if="pending">
+    <span class="loading loading-spinner loading-md"></span>
+  </div>
+  <div v-else>
+    <EventiLista :eventi="data.items" />
+  </div>
+</template>
---
+++ components/blog/Post.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps(["post"]);
+
+const breadcrumb = computed(() => [
+  {
+    label: "blog",
+    path: "/blog",
+  },
+  {
+    label: props.post.titolo,
+  },
+]);
+
+useHead({
+  title: props?.post?.titolo + " | Bitcoin Italia Network",
+  link: [
+    {
+      rel: "canonical",
+      href: `https://bitcoinitalianetwork.com/b/${props?.post?.slug}`,
+    },
+  ],
+});
+</script>
+
+<template>
+  <LandingSectionhead>
+    <template v-slot:title>{{ post?.titolo }}</template>
+  </LandingSectionhead>
+  <LandingBreadcrumb :voci="breadcrumb" />
+
+    <div v-html="post.corpo" class="mt-3"></div>
+</template>
