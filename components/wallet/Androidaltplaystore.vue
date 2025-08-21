042f733c912089f39f2fd6ab449b93514afde59e
Mauro Pili <mauro.pili@gmail.com>
2025-08-21 12:52:37 +0200

    WalletAndroidaltplaystore

---
+++ components/wallet/Androidaltplaystore.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+    att: {
+      required: false,
+    },
+    data: {
+      required: true,
+    },
+	campo: {
+	  type: String,
+	  required: true,
+	},
+  },
+};
+</script>
+
+<template>
+	  <tr
+    v-if="att.campo == 'Android' && data[campo]"
+    class="border-b border-gray-200"
+  >
+    <td></td>
+    <td>{{campo}}</td>
+    <td>
+      <span class="text-blue-600 text-sm">
+        <a :href="data[campo]" target="_blank">
+          {{ data[campo] }}
+        </a>
+      </span>
+    </td>
+  </tr>
+</template>
