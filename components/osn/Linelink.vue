0edd8e3ad4bb2c3970b1a0df876d99487ff3d44b
Mauro Pili <mauro.pili@gmail.com>
2025-10-17 17:40:46 +0200

    slider distanza e indicator nella lista

--- components/osn/Linelink.vue
+++ components/osn/Linelink.vue
@@ -3,27 +3,32 @@ defineProps(["poi"]);
 </script>
 
 <template>
+  <div v-if="poi?.tags">
   <NuxtLink :to="usePoiLink(poi)">
-    <div v-if="poi?.tags" class="flex flex-wrap p-2 border-1 my-2 rounded shadow">
-      <div class="text-primary font-bold pr-2">
-        {{ poi?.tags?.name }}
+      <div class="indicator">
+        <span v-if="poi?.tags['currency:XBT']=='yes'" class="indicator-item indicator-start status status-warning"></span>
+        <div class="flex flex-wrap p-2 border-1 my-2 rounded shadow">
+          <div class="text-primary font-bold pr-2">
+            {{ poi?.tags?.name }}
+          </div>
+          <div class="pr-2">
+            <span v-if="poi?.tags['addr:place']">
+              {{ poi.tags["addr:place"] }}&nbsp;
+            </span>
+            <span v-if="poi.tags['addr:district']">
+              {{ poi.tags["addr:district"] }}&nbsp;
+            </span>
+            <OsnStreetNum :tags="poi?.tags" />
+          </div>
+          <div class="text-accent pr-2">
+            <OsnCitta :tags="poi?.tags" />
+          </div>
+          <!-- <div>{{poi}}</div> -->
+          <!-- <div class="text-gray-400">
+            <OsnTipovenue :tags="poi?.tags" />
+          </div> -->
+        </div>
       </div>
-      <div class="pr-2">
-        <span v-if="poi?.tags['addr:place']">
-          {{ poi.tags["addr:place"] }}&nbsp;
-        </span>
-        <span v-if="poi.tags['addr:district']">
-          {{ poi.tags["addr:district"] }}&nbsp;
-        </span>
-        <OsnStreetNum :tags="poi?.tags" />
-      </div>
-      <div class="text-accent pr-2">
-        <OsnCitta :tags="poi?.tags" />
-      </div>
-      <!-- <div>{{poi}}</div> -->
-      <!-- <div class="text-gray-400">
-        <OsnTipovenue :tags="poi?.tags" />
-      </div> -->
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+  </div>
 </template>
