ab2ef78748007b09136d5a4e8b1c9fd0a0279c0b
Mauro Pili <mauro.pili@gmail.com>
2026-02-27 08:29:25 +0100

    reorg nostr test

--- pages/nostr.vue
+++ pages/nostr.vue
@@ -14,20 +14,27 @@ definePageMeta({
   <LandingContainer>
     <h1>Benvenuto nella pagina di test dei componenti Nostr</h1>
     <ClientOnly>
+      <hr />
+      <h2>Nostr zap button</h2>
       <nostr-zap-button
         nip05="bin@bitcoinitalianetwork.com"
         url="https://bitcoinitalianetwork.com/nostr"
         relays="wss://relay.primal.net,wss://relay.damus.io,wss://relay.nostr.band,wss://nos.lol"
       ></nostr-zap-button>
       <hr />
+      <h2>Nostr like button</h2>
+      <nostr-like-button
+        relays="wss://relay.primal.net,wss://relay.damus.io,wss://relay.nostr.band,wss://nos.lol"
+      ></nostr-like-button>
+      <hr />
+
+      <hr />
+      <h2>Nostr profile</h2>
       <nostr-profile
         npub="npub1s0mv35668yg7t4jk49zdltkezn0em0s3jpt2qn8mkneurnw2edws0uy9zw"
         relays="wss://relay.primal.net,wss://relay.damus.io,wss://relay.nostr.band,wss://nos.lol"
       ></nostr-profile>
       <hr />
-      <nostr-like-button
-        relays="wss://relay.primal.net,wss://relay.damus.io,wss://relay.nostr.band,wss://nos.lol"
-      ></nostr-like-button
-    ></ClientOnly>
+    </ClientOnly>
   </LandingContainer>
 </template>
