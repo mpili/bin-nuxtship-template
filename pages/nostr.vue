78af385fcd1b47f470b557c5f6af91432e6bb1d1
Mauro Pili <mauro.pili@gmail.com>
2026-02-27 18:12:31 +0100

    reorg test nostr

--- pages/nostr.vue
+++ pages/nostr.vue
@@ -1,8 +1,10 @@
 <script setup>
 const isDev = process.dev;
 
+const titolo = "Pagina test componenti Nostr - [a]"
+
 useHead({
-  title: "pagina di test per componenti nostr",
+  title: titolo,
 });
 
 definePageMeta({
@@ -17,59 +19,63 @@ const pubkey="83f6c8d35a3911e5d656a944dfaed914df9dbe119056a04cfbb4f3c1cdcacb5d";
 
 <template>
   <LandingContainer>
-    <h1>Pagina di test dei componenti Nostr</h1>
-    <ClientOnly>
+    <h1>{{titolo}}</h1>
       <hr />
 
       <h2>Zap button</h2>
-      <!-- nip05="bin@bitcoinitalianetwork.com" -->
-      <nostr-zap-button
-        :npub="npub"
-        url="https://bitcoinitalianetwork.com/nostr"
-        :relays="relays"
-      ></nostr-zap-button>
-
-      <hr />
+      <ClientOnly>
+        <nostr-zap-button
+          :npub="npub"
+          url="https://bitcoinitalianetwork.com/nostr"
+          :relays="relays"
+        ></nostr-zap-button>
+        <hr />
+      </ClientOnly>
 
       <h2>Like button</h2>
-      <nostr-like-button
-        :relays="relays"
-      ></nostr-like-button>
-      <hr />
+      <ClientOnly>
+        <nostr-like-button
+          :relays="relays"
+        ></nostr-like-button>
+        <hr />
+      </ClientOnly>
 
-      <h2>Post</h2>
-
-      <nostr-post
-        eventId="nevent1qqswfvks5gwxamgqjdujp5f0u3jr0zyxjdjjq65avc6sl0uwcwhh7fgpzemhxue69uhhyetvv9ujuurjd9kkzmpwdejhgq3qs0mv35668yg7t4jk49zdltkezn0em0s3jpt2qn8mkneurnw2edwsenydjd"
-        :relays="relays"
-      ></nostr-post>
-
-      <hr />
+      <!-- <h2>Post</h2>
+      <ClientOnly>
+        <nostr-post
+          eventId="nevent1qqswfvks5gwxamgqjdujp5f0u3jr0zyxjdjjq65avc6sl0uwcwhh7fgpzemhxue69uhhyetvv9ujuurjd9kkzmpwdejhgq3qs0mv35668yg7t4jk49zdltkezn0em0s3jpt2qn8mkneurnw2edwsenydjd"
+          :relays="relays"
+        ></nostr-post>
+        <hr />
+      </ClientOnly> -->
 
       <h2>Follow button</h2>
-      <nostr-follow-button
-        :npub="npub"
-        :relays="relays"
-      ></nostr-follow-button>
-
-      <hr />
+      <ClientOnly>
+        <nostr-follow-button
+          :npub="npub"
+          :relays="relays"
+          ></nostr-follow-button>
+          <hr />
+      </ClientOnly>
 
       <h2>Badge</h2>
       <!-- pubkey="npub1s0mv35668yg7t4jk49zdltkezn0em0s3jpt2qn8mkneurnw2edws0uy9zw" -->
-      <nostr-profile-badge
-        :npub="npub"
-        :relays="relays"
-      ></nostr-profile-badge>
-
-      <hr />
+      <ClientOnly>
+        <nostr-profile-badge
+          :npub="npub"
+          :relays="relays"
+        ></nostr-profile-badge>
+        <hr />
+      </ClientOnly>
 
       <h2>Profile</h2>
-      <nostr-profile
-        :npub="npub"
-        :relays="relays"
-      ></nostr-profile>
+      <ClientOnly>
+        <nostr-profile
+          :npub="npub"
+          :relays="relays"
+        ></nostr-profile>
+        <hr />
+      </ClientOnly>
 
-      <hr />
-    </ClientOnly>
   </LandingContainer>
 </template>
